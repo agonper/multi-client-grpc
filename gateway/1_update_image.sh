b7a64bfdc05c9dec3e6de624e956c362bc2e049f
Alberto González <alberto.gonzalez@uji.es>
2020-06-04 13:01:03 +0200

    Fix gRPC-web error by updating ESP to v2.11

--- gateway/1_update_image.sh
+++ gateway/1_update_image.sh
@@ -1,4 +1,4 @@
 ./gcloud_build_image \
     -s api-gateway-3t5ec74iwa-ew.a.run.app \
-    -c 2020-05-29r0 \
+    -c 2020-06-04r0 \
     -p multi-client-grpc
--- gateway/2_deploy_gateway.sh
+++ gateway/2_deploy_gateway.sh
@@ -1,5 +1,5 @@
 gcloud run deploy api-gateway \
-      --image="eu.gcr.io/multi-client-grpc/endpoints-runtime-serverless:api-gateway-3t5ec74iwa-ew.a.run.app-2020-05-29r0" \
+      --image="eu.gcr.io/multi-client-grpc/endpoints-runtime-serverless:api-gateway-3t5ec74iwa-ew.a.run.app-2020-06-04r0" \
       --allow-unauthenticated \
       --platform managed \
       --project multi-client-grpc
